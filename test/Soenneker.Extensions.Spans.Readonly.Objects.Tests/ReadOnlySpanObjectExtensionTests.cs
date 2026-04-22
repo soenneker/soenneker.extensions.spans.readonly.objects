82976f681fa612229f6861674e493e444dad5672
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:04 -0500

    TUnit conversion

--- test/Soenneker.Extensions.Spans.Readonly.Objects.Tests/ReadOnlySpanObjectExtensionTests.cs
+++ test/Soenneker.Extensions.Spans.Readonly.Objects.Tests/ReadOnlySpanObjectExtensionTests.cs
@@ -1,11 +1,10 @@
 using Soenneker.Tests.Unit;
-using Xunit;
 
 namespace Soenneker.Extensions.Spans.Readonly.Objects.Tests;
 
 public sealed class ReadOnlySpanObjectExtensionTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
